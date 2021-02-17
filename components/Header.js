bd394c1f3f07c4e239815e5a3708f1edef8d814f
0elle0 <el_leo_m@hotmail.com>
2021-02-17 21:30:20 +0200

    fix Header color

--- components/Header.js
+++ components/Header.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: 'red',
+        backgroundColor: 'rgba(232,82,100,1)',
         alignItems: 'center',
         justifyContent: 'center',
         
