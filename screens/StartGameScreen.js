e76fd4bfc43b91e1145c6a87945568a9383a4c12
0elle0 <el_leo_m@hotmail.com>
2021-02-18 20:13:27 +0200

    input validation OK, added TouchWithoutFeedback

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	Button,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from "react-native";
 import Card from "../components/Card.js";
 import Colors from "../constants/colors.js";
 import Input from "../components/Input.js";
@@ -9,37 +16,43 @@ import Input from "../components/Input.js";
 const StartGameScreen = (props) => {
 	const [enteredValue, setEnteredValue] = useState("");
 
+	//uses regex to delete any non numeric value entered
 	const numberInputHandler = (inputText) => {
-		setEnteredValue(() => {
-			
-		})
+		setEnteredValue(inputText.replace(/[^0-9]/g, ""));
 	};
 
 	return (
-		<View style={styles.screen}>
-			<Text style={styles.title}>Game Start!</Text>
-			<Card style={styles.inputContainer}>
-				<Text>Select a number!</Text>
-				<Input
-					style={styles.input}
-					blurOnSubmit
-					keyboardType='number-pad'
-					autoCorrect={false}
-					text='Open'
-					maxLength={2}
-					onChangeText={numberInputHandler}
-					value={enteredValue}
-				/>
-				<View style={styles.buttongrp}>
-					<View style={styles.button}>
-						<Button color={Colors.accent} title='RESET' />
+		// allows to close the keyboard once the user touches outside
+		// of the input area
+		<TouchableWithoutFeedback
+			onPress={() => {
+				Keyboard.dismiss();
+			}}>
+			<View style={styles.screen}>
+				<Text style={styles.title}>Game Start!</Text>
+				<Card style={styles.inputContainer}>
+					<Text>Select a number!</Text>
+					<Input
+						style={styles.input}
+						blurOnSubmit
+						keyboardType='number-pad'
+						autoCorrect={false}
+						text='Open'
+						maxLength={2}
+						onChangeText={numberInputHandler}
+						value={enteredValue}
+					/>
+					<View style={styles.buttongrp}>
+						<View style={styles.button}>
+							<Button color={Colors.accent} title='RESET' />
+						</View>
+						<View style={styles.button}>
+							<Button color={Colors.primary} title='CONFIRM' />
+						</View>
 					</View>
-					<View style={styles.button}>
-						<Button color={Colors.primary} title='CONFIRM' />
-					</View>
-				</View>
-			</Card>
-		</View>
+				</Card>
+			</View>
+		</TouchableWithoutFeedback>
 	);
 };
 
