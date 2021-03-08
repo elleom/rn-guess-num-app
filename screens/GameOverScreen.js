1f4e220398a1901b98597957c7cc92e3795d66d2
0elle0 <el_leo_m@hotmail.com>
2021-03-08 21:20:04 +0100

    styled image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,27 +1,48 @@
-import React from 'react';
-import { TextPropTypes } from 'react-native';
-import {View, Text, StyleSheet, Button, Image} from 'react-native';
-import TitleText from '../components/TitleText';
+import React from "react";
+import { TextPropTypes } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+import TitleText from "../components/TitleText";
 
-const GameOverScreen = props => {
-    return(
-        <View style={styles.screen}>
-            <TitleText>Game is over</TitleText>
-            <Image source={require('../assets/images/success.png')} />
-            <Text>Number of rounds : {props.roundsNumber}</Text>
-            <Text>Number was : {props.userNumber}</Text>
-            <Button title='RESTART'  onPress={props.onRestart}/>
-        </View>
-    )
-}
+const GameOverScreen = (props) => {
+	return (
+		<View style={styles.screen}>
+			<TitleText>Game is over</TitleText>
+			<View style={styles.imageContainer}>
+				<Image
+					source={require("../assets/images/success.png")}
+					style={styles.image}
+				/>
+			</View>
+			<Text>Number of rounds : {props.roundsNumber}</Text>
+			<Text>Number was : {props.userNumber}</Text>
+			<Button title='RESTART' onPress={props.onRestart} />
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-
-    }
-})
+	screen: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+    imageContainer: 
+    {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        overflow: "hidden",
+        borderColor: 'black',
+        marginVertical: 20,
+    },
+	image: {
+		width: "100%",
+		height: "100%",
+		
+         
+	},
+    
+});
 
 export default GameOverScreen;
